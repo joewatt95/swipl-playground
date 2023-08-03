f07f5249e9c0e2012a3f127a5a4940c733447db5
joewatt95 <joewatt95@u.nus.edu>
2023-08-03 10:20:36 +0800

    Bump npm and browser deps

--- src/swipl_playground/view.cljs
+++ src/swipl_playground/view.cljs
@@ -34,14 +34,14 @@
 (h/defelem html [_attrs _children]
   (h/div
    (h/link :rel "stylesheet"
-           :href "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+           :href "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
            :integrity "sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
            :crossorigin "anonymous")
     ;;  (h/script :src "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
     ;;            :integrity "sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
     ;;            :crossorigin "anonymous")
     ;; SWIPL stuff
-   (h/script :src "https://SWI-Prolog.github.io/npm-swipl-wasm/3/4/5/index.js")
+   (h/script :src "https://SWI-Prolog.github.io/npm-swipl-wasm/3/4/7/index.js")
 
    (h/title "SWI-Prolog playground")
    (h/h1 "SWI-Prolog playground")
